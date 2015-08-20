7ee664ffd038e654b097b75b6d563efcea282359
TioDuke <tioduke@gmail.com>
2015-08-20 15:34:18 -0400

    add tests legendary

---
+++ GuildedRose/src/com/gildedrose/tests/LegendaryItemTest.java
@@ -0,0 +1,87 @@
+package com.gildedrose.tests;
+
+import com.gildedrose.items.*;
+import com.gildedrose.GildedRose;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class LegendaryItemTest {
+
+    @Test
+    public void updateQuality_SellinGreterThanZero_QualityUnchanged() {
+    	//Arrange
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        
+        //Act
+        app.updateQuality();
+        
+        //Assert
+        assertEquals("Sulfuras, Hand of Ragnaros", items[0].name);
+        assertEquals(10, items[0].sellIn);
+        assertEquals(20, items[0].quality);
+    }
+
+    @Test
+    public void updateQuality_SellinEqualsZero_QualityUnchanged() {
+    	//Arrange
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        
+        //Act
+        app.updateQuality();
+        
+        //Assert
+        assertEquals("Sulfuras, Hand of Ragnaros", items[0].name);
+        assertEquals(0, items[0].sellIn);
+        assertEquals(20, items[0].quality);
+    }
+
+    @Test
+    public void updateQuality_SellinLessThanZero_QualityUnchanged() {
+    	//Arrange
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 20) };
+        GildedRose app = new GildedRose(items);
+        
+        //Act
+        app.updateQuality();
+        
+        //Assert
+        assertEquals("Sulfuras, Hand of Ragnaros", items[0].name);
+        assertEquals(-1, items[0].sellIn);
+        assertEquals(20, items[0].quality);
+    }
+
+    @Test
+    public void updateQuality_QualityEqualsZero_QualityUnchanged() {
+    	//Arrange
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 0) };
+        GildedRose app = new GildedRose(items);
+        
+        //Act
+        app.updateQuality();
+        
+        //Assert
+        assertEquals("Sulfuras, Hand of Ragnaros", items[0].name);
+        assertEquals(10, items[0].sellIn);
+        assertEquals(0, items[0].quality);
+    }
+
+    @Test
+    public void updateQuality_QualityEqualsFifty_QualityUnchanged() {
+    	//Arrange
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        
+        //Act
+        app.updateQuality();
+        
+        //Assert
+        assertEquals("Sulfuras, Hand of Ragnaros", items[0].name);
+        assertEquals(10, items[0].sellIn);
+        assertEquals(50, items[0].quality);
+    }
+
+}
