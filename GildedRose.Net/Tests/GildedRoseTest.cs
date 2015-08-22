fd767b42551ea4ce7131254c299f73b2a5b255c9
TioDuke <tioduke@gmail.com>
2015-08-22 14:37:23 -0400

    add .Net project

---
+++ GildedRose.Net/Tests/GildedRoseTest.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Xunit;
+
+//using GildedRose;
+//using GildedRose.Items;
+
+namespace GildedRose.Tests
+{
+	public class GildedRoseTest
+	{
+		[Fact]
+		public void foo() {
+			IList<Item> Items = new List<Item> { new Item{Name = "foo", SellIn = 0, Quality = 0} };
+			GildedRose app = new GildedRose(Items);
+			app.UpdateQuality();
+			Assert.Equal("fixme", Items[0].Name);
+		}
+	}
+}
