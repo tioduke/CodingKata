5898d5985a86079f0e245fffc74f6e6691d7b54b
TioDuke <tioduke@gmail.com>
2019-12-15 02:47:41 -0500

    code coverage

--- GildedRose.Net/GildedRose.Net/Items/ItemDecorator.cs
+++ GildedRose.Net/GildedRose.Net/Items/ItemDecorator.cs
@@ -6,7 +6,7 @@ namespace GildedRose.Net.Items
         // Constants
         protected const int MIN_QUALITY = 0;
         protected const int MAX_QUALITY = 50;
-    
+
         // Members
         private Item itemToDecorate;
 
@@ -15,20 +15,8 @@ namespace GildedRose.Net.Items
         {
             this.itemToDecorate = itemToDecorate;
         }
-        
+
         // Properties
-        public string Name
-        {
-            get
-            {
-                return this.itemToDecorate.Name;
-            }
-            set
-            {
-                this.itemToDecorate.Name = value;
-            }
-        }
-        
         public int SellIn
         {
             get
@@ -40,7 +28,7 @@ namespace GildedRose.Net.Items
                 this.itemToDecorate.SellIn = value;
             }
         }
-        
+
         public int Quality
         {
             get
@@ -52,7 +40,7 @@ namespace GildedRose.Net.Items
                 this.itemToDecorate.Quality = value;
             }
         }
-    
+
         // Read-only properties
         protected bool SellInEnded
         {
@@ -60,66 +48,66 @@ namespace GildedRose.Net.Items
                 return this.SellIn <= 0;
             }
         }
-        
+
         protected bool SellInEvenNumber
         {
             get {
                 return this.SellIn % 2 == 0;
             }
         }
-        
+
         protected bool SellInOddNumber
         {
             get {
                 return this.SellIn % 2 != 0;
             }
         }
-        
+
         // Public methods
         public void Update()
         {
             this.UpdateQuality();
             this.UpdateSellIn();
         }
-        
+
         // Protected methods
         protected abstract void UpdateQuality();
-        
+
         protected virtual void UpdateSellIn()
         {
             this.SellIn--;
         }
-        
+
         protected void DecreaseQualityOnce()
         {
             this.Quality -= 1;
             if (this.Quality < MIN_QUALITY) this.Quality = MIN_QUALITY;
         }
-        
+
         protected void DecreaseQualityTwice()
         {
             this.Quality -= 2;
             if (this.Quality < MIN_QUALITY) this.Quality = MIN_QUALITY;
         }
-        
+
         protected void DecreaseQualityQuarce()
         {
             this.Quality -= 4;
             if (this.Quality < MIN_QUALITY) this.Quality = MIN_QUALITY;
         }
-        
+
         protected void IncreaseQualityOnce()
         {
             this.Quality += 1;
             if (this.Quality > MAX_QUALITY) this.Quality = MAX_QUALITY;
         }
-        
+
         protected void IncreaseQualityTwice()
         {
             this.Quality += 2;
             if (this.Quality > MAX_QUALITY) this.Quality = MAX_QUALITY;
         }
-        
+
         protected void IncreaseQualityThrice()
         {
             this.Quality += 3;
